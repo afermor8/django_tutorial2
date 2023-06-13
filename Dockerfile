c9409664b9d9a8f44b0c0d4877ad1972b574f9bd
afermor8 <ara.fer.mor@gmail.com>
2023-06-13 11:56:05 +0200

    He modificado Dockerfile y añadido script

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,16 @@
-FROM debian
-RUN apt-get update && apt-get install -y apache2 && apt-get clean && rm -rf /var/lib/apt/lists/*
+FROM python:3
+WORKDIR /usr/src/app
+MAINTAINER Arantxa Fernández Morató 'ara.fer.mor@gmail.com'
+RUN apt-get install git && pip install --root-user-action=ignore --upgrade pip && pip install --root-user-action=ignore django mysqlclient
+RUN git clone https://github.com/afermor8/django_tutorial.git /usr/src/app && mkdir static
+ADD ./script.sh /usr/src/app/
+RUN chmod +x /usr/src/app/script.sh
+ENV ALLOWED_HOSTS=*
+ENV DJANGO_HOST=mariadb
+ENV DJANGO_USER=django
+ENV DJANGO_PASSWORD=django
+ENV DJANGO_DB=django
+ENV DJANGO_SUPERUSER_PASSWORD=admin
+ENV DJANGO_SUPERUSER_USERNAME=admin
+ENV DJANGO_SUPERUSER_EMAIL=admin@example.org
+ENTRYPOINT ["/usr/src/app/script.sh"]
--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
 
 # Application definition
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'django',
+#        'USER': 'django',
+#        'PASSWORD': 'djando',
+#        'HOST': 'mariadb',
+#        'PORT': '3306',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -118,6 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+CSRF_TRUSTED_ORIGINS = ['http://*.afm-tars.es','http://*.127.0.0.1','https://*.afm-tars.es','https://*.127.0.0.1']
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
